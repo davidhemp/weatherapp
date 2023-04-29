d9191b4a43d3aa29a5e8b610b760cd3c8c0d9dd8
Veio <cyber_veio@hotmail.com>
2023-04-29 15:50:50 +0100

    Working js with icons

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const error404 = document.querySelector('.not-found');
 search.addEventListener('click', () => {
     // Insecure but there you go
     const APIKey = 'a6c1d018ca65fd099d9bdb4bcd15da6b';
-    // const city = document.querySelector('.search-box input').value;
-    const city = 'London'
+    const city = document.querySelector('.search-box input').value;
     
     if (city === '') {
         // Default style
@@ -33,19 +32,32 @@ search.addEventListener('click', () => {
             }
             
             if (json.cod === 200){
+                // Grab weather details elements
                 const image = document.querySelector('.weather-box img');
                 const temperature = document.querySelector('.weather-box .temperature');
                 const description = document.querySelector('.weather-box .description');
                 const humidity = document.querySelector('.weather-details .humidity span');
                 const wind = document.querySelector('.weather-details .wind span');
-
+                
+                //Ensure 404 is hidden and display weather box
                 error404.style.display = 'none';
-                container.style.height = '590px';    
+                container.style.height = '550px';    
                 weatherBox.style.display = '';
                 weatherDetails.style.display = '';
                 weatherBox.classList.add('fadeIn');
                 weatherDetails.classList.add('fadeIn');
 
+                //Set values and images. Assuming a static name but using a failback if we can't find a matching image
+                fetch(`images/${json.weather[0].main}.png`, { method :'HEAD'})
+                    .then(res => {
+                        if(res.ok){
+                            image.src = `images/${json.weather[0].main}.png`;
+                        } else {
+                            image.src = 'images/general.png';
+                        }
+                    })
+                
+                description.innerHTML = `${json.weather[0].description}`;
                 humidity.innerHTML = `${json.main.humidity}%`;
                 temperature.innerHTML = `${parseInt(json.main.temp)}°C`;
                 wind.innerHTML = `${json.wind.speed}Km/h`;
